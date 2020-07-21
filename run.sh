a38f81500cac63d7c5fd482ed75e4b7656cfd9e0
Ed Baskerville <ed@edbaskerville.com>
2020-07-21 12:38:27 -0500

    Added -Xmx=1500m to java command

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 ROOT_DIR=`dirname $0`
-java -cp "$ROOT_DIR/bin:$ROOT_DIR/colt/colt.jar:$ROOT_DIR/gson/gson-2.8.6.jar:$ROOT_DIR/junit/junit-4.13.jar" \
+java -Xmx1500m -cp "$ROOT_DIR/bin:$ROOT_DIR/colt/colt.jar:$ROOT_DIR/gson/gson-2.8.6.jar:$ROOT_DIR/junit/junit-4.13.jar" \
     landusemodel.Main $@
