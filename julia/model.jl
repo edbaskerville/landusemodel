815dad69ca1814ce79b7580c6870ad3b0b4f4755
Ed Baskerville <ed@edbaskerville.com>
2020-09-24 09:40:32 -0700

    Rewrote F->H and F->A in terms of sampling sites in state H rather than state F

--- julia/model.jl
+++ julia/model.jl
@@ -19,40 +19,40 @@ end
 
 mutable struct Parameters
     rng_seed::Union{Int64, Nothing}
-    
+
     L::Int64
-    
+
     t_final::Float64
     t_output::Float64
-    
+
     max_rate_FH::Float64
     frac_global_FH::Float64
-    
+
     max_rate_AD::Float64
     min_rate_frac_AD::Float64
-    
+
     max_rate_HD::Float64
     min_rate_frac_HD::Float64
-    
+
     rate_DF::Float64
-    
+
     beta_initial::Float64
     sd_log_beta::Float64
-    
+
     probability_function_FH::FHProbabilityFunction
-    
+
     enable_animation::Bool
     t_animation_frame::Float64
-    
+
     H_color::Vector{Float64}
     A_color::Vector{Float64}
     F_color::Vector{Float64}
     D_color::Vector{Float64}
-    
+
     beta_bg_color::Vector{Float64}
     beta_min_color::Vector{Float64}
     beta_max_color::Vector{Float64}
-    
+
     beta_image_max::Float64
 
     function Parameters()
@@ -94,20 +94,20 @@ struct Site
     t_init::Float64
     state::Int64
     t_beta::Union{TBetaPair, Nothing}
-    
+
     function Site()
         Site(0.0, 0)
     end
-    
+
     function Site(t::Float64, state::Int64)
         Site(t, state, nothing)
     end
-    
+
     function Site(t::Float64, state::Int64, t_beta::Union{TBetaPair, Nothing})
         if t_beta === nothing
             @assert state != H
         end
-        
+
         new(t, state, t_beta)
     end
 end
@@ -116,43 +116,43 @@ mutable struct Simulation
     params::Parameters
     rng::MersenneTwister
     t::Float64
-    
+
     sites::Matrix{Site}
     locs::Matrix{Loc}
     sites_by_state::Vector{ArraySet{Loc}}
-    
+
     max_beta::Float64
-    
+
     event_rates::Vector{Float64}
     event_weights::Weights
-    
+
     lifetime_sums::Vector{Float64}
     lifetime_counts::Vector{Int64}
-    
+
     function Simulation(params::Parameters)
         s = new()
-        
+
         s.params = deepcopy(params)
         if s.params.rng_seed === nothing
             s.params.rng_seed = Int64(rand(RandomDevice(), UInt32))
         end
         p = s.params
-        
+
         s.rng = MersenneTwister(s.params.rng_seed)
-        
+
         s.t = 0.0
         s.sites = Matrix{Site}(undef, p.L, p.L)
         s.locs = Matrix{Loc}(undef, p.L, p.L)
         s.sites_by_state = Vector{ArraySet{Loc}}(length(STATES))
         s.max_beta = p.beta_initial
-        
+
         s.lifetime_sums = zeros(Float64, length(STATES))
         s.lifetime_counts = zeros(Int64, length(STATES))
-        
+
         s.event_rates = repeat([0.0], length(EVENTS))
-        
+
         initialize!(s)
-        
+
         s
     end
 end
@@ -160,25 +160,25 @@ end
 function initialize!(s::Simulation)
     p = s.params
     L = p.L
-    
+
     # Initialize one human site in the center of the grid
     locH = div(L, 2)
     set_state!(s, (locH, locH), H, TBetaPair(0.0, p.beta_initial))
-    
-    # Initialize every other site as forest 
+
+    # Initialize every other site as forest
     for j in 1:L
         for i in 1:L
             s.locs[i,j] = (i, j)
-            
+
             if !(i == locH && j == locH)
                 set_state!(s, (i, j), F)
             end
         end
     end
-    
+
     @assert length(s.sites_by_state[H]) == 1
     @assert length(s.sites_by_state[F]) == L * L - 1
-    
+
     update_rates!(s)
 end
 
@@ -199,22 +199,22 @@ end
 
 function set_state!(s::Simulation, loc::Tuple{Int64, Int64}, state::Int64, t_beta::Union{TBetaPair, Nothing})
     @assert state != 0
-    
+
     loc_index = CartesianIndex(loc)
     site = s.sites[loc_index]
-    
+
     # Remove from sites_by_state set for old state; track lifetime
     if site.state != 0 && site.state != state
         remove!(s.sites_by_state[site.state], loc)
         s.lifetime_sums[site.state] += s.t - site.t_init
         s.lifetime_counts[site.state] += 1
     end
-    
+
     # Insert into sites_by_state set for new state
     if site.state == 0 || site.state != state
         insert!(s.sites_by_state[state], loc)
     end
-    
+
     if site.state == state
         s.sites[loc_index] = Site(site.t_init, state, t_beta)
     else
@@ -223,11 +223,11 @@ function set_state!(s::Simulation, loc::Tuple{Int64, Int64}, state::Int64, t_bet
 end
 
 function get_beta!(s::Simulation, loc::Tuple{Int64, Int64})
-    @debug "get_beta!", s.t, loc
-    
+    # @debug "get_beta!", s.t, loc
+
     site = get_site(s, loc)
     @assert site.state == H
-    
+
     if s.t > site.t_beta.t
         dt = s.t - site.t_beta.t
         p = s.params
@@ -237,13 +237,13 @@ function get_beta!(s::Simulation, loc::Tuple{Int64, Int64})
         # (variance is proportional to timestep, inversely proportional to rate)
         sd_dt = sqrt(p.sd_log_beta^2 * dt)
         beta = site.t_beta.beta * exp(randn(rng) * sd_dt)
-        
+
         set_state!(s, loc, H, TBetaPair(s.t, beta))
-        
+
         if beta > s.max_beta
             s.max_beta = beta
         end
-        
+
         beta
     else
         site.t_beta.beta
@@ -264,7 +264,7 @@ end
 
 function simulate(s::Simulation)
     p = s.params
-    
+
     if p.enable_animation
         if isdir("state_images")
             error("state_images already exists; delete first")
@@ -275,12 +275,12 @@ function simulate(s::Simulation)
         mkdir("state_images")
         mkdir("beta_images")
     end
-    
+
     if isfile("output.sqlite")
         error("output.sqlite already exists; delete first")
     end
     db = SQLite.DB("output.sqlite")
-    
+
     DBInterface.execute(db, """
         CREATE TABLE output (
             time REAL,
@@ -307,24 +307,26 @@ function simulate(s::Simulation)
             beta_975 REAL
         )
     """)
-    
+
     # Repeatedly do events
     t_next_output = 0.0
     t_next_frame = 0.0
     while s.t < p.t_final
         R = sum(s.event_rates)
-        
+
         # Draw next event time using total rate
-        @debug "event_rates" s.event_rates
+        # @debug "event_rates" s.event_rates
         t_next = if R == 0.0
+            @info "R == 0.0; ending simulation"
             p.t_final
         else
             s.t + randexp(s.rng) / sum(s.event_weights)
         end
-        @debug "t_next" t_next
-        
+        # @debug "t_next" t_next
+
         # If the next event is after the output time, we need to do some output
         while t_next >= t_next_output
+            s.t = t_next_output
             betas = get_betas!(s)
             do_output(s, db, t_next_output, betas)
             if p.enable_animation && t_next_output == t_next_frame
@@ -338,15 +340,20 @@ function simulate(s::Simulation)
                 )
                 t_next_frame += p.t_animation_frame
             end
-            t_next_output += p.t_output
+
+            if R == 0.0
+                break
+            else
+                t_next_output += p.t_output
+            end
         end
-        
+
         s.t = t_next
-        
+
         if R > 0.0
             # Sample next event category proportional to event rate
             event_id = sample(s.rng, EVENTS, s.event_weights)
-            @debug "event_id" event_id
+            # @debug "event_id" event_id
 
             # Perform event and update all rates
             event_occurred = do_event!(event_id, s, t_next)
@@ -359,12 +366,12 @@ end
 
 function make_state_image(s::Simulation)
     p = s.params
-    
+
     H_color = RGB(p.H_color...)
     A_color = RGB(p.A_color...)
     F_color = RGB(p.F_color...)
     D_color = RGB(p.D_color...)
-    
+
     function convert(x)
         if x.state == H
             H_color
@@ -376,7 +383,7 @@ function make_state_image(s::Simulation)
             D_color
         end
     end
-    
+
     convert.(s.sites)
 end
 
@@ -395,28 +402,28 @@ end
 
 function make_beta_image!(s::Simulation, betas)
     p = s.params
-    
+
     bg_color = RGB(p.beta_bg_color...)
-    
+
     function convert(loc)
         site = get_site(s, loc)
         if site.state == H
             beta = get_beta!(s, loc)
             value01 = clamp01(beta / p.beta_image_max)
             rgb = p.beta_min_color .+ (p.beta_max_color .- p.beta_min_color) .* value01
-            
+
             RGB(rgb...)
         else
             bg_color
         end
     end
-    
+
     convert.(s.locs)
 end
 
 function do_output(s::Simulation, db, t_output, betas)
     println("Outputting at ", t_output)
-    
+
     beta_mean = mean(betas)
     beta_sd = std(betas, corrected = false)
     beta_min = if length(betas) == 0
@@ -434,7 +441,7 @@ function do_output(s::Simulation, db, t_output, betas)
     else
         quantile(betas, [0.025, 0.05, 0.10, 0.25, 0.5, 0.75, 0.9, 0.95, 0.975])
     end
-    
+
     DBInterface.execute(db, """
         INSERT INTO output VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
     """, vcat(
@@ -455,7 +462,7 @@ function do_output(s::Simulation, db, t_output, betas)
 end
 
 function get_lifetime_avg(s::Simulation, state::Int64)
-    s.lifetime_sums[state] / s.lifetime_counts[state]    
+    s.lifetime_sums[state] / s.lifetime_counts[state]
 end
 
 function update_rates!(s::Simulation)
@@ -512,45 +519,41 @@ end
 
 function get_rate_local_FH(s::Simulation)
     p = s.params
-    
-    if state_count(s, H) == 0
-        0.0
-    else
-        (1.0 - p.frac_global_FH) * state_count(s, F) * p.max_rate_FH
-    end
+
+    (1.0 - p.frac_global_FH) * state_count(s, H) * p.max_rate_FH
 end
 
 function do_event_local_FH!(s::Simulation, t)
-    @debug "do_event_local_FH!", t
-    
+    # @debug "do_event_local_FH!", t
+
     p = s.params
     rng = s.rng
-    
-    # Draw a random forested site to be colonized
-    @assert state_count(s, F) > 0
-    loc_F = draw_location_in_state(s, F)
-    @debug "loc_F", loc_F
-    
-    # Draw a random neighbor of the forested site
-    loc_neighbor = draw_neighbor(s, loc_F)
-    @debug "loc_neighbor", loc_neighbor
-    
-    # If neighbor is in state H, maybe transition to state H
-    @debug "neighbor state" get_state(s, loc_neighbor)
-    if get_state(s, loc_neighbor) == H
-        @debug "neighbor is H"
-        
+
+    # Draw a random inhabited site to do the colonizing
+    @assert state_count(s, H) > 0
+    loc_H = draw_location_in_state(s, H)
+    # @debug "loc_H", loc_H
+
+    # Draw a random neighbor of the inhabited site
+    loc_neighbor = draw_neighbor(s, loc_H)
+    # @debug "loc_neighbor", loc_neighbor
+
+    # If neighbor is in state F, maybe transition to state H
+    # @debug "neighbor state" get_state(s, loc_neighbor)
+    if get_state(s, loc_neighbor) == F
+        # @debug "neighbor is H"
+
         # Perform event with probability that depends on neighbors of H
-        if rand(rng) < probability_FH(s, loc_neighbor)
+        if rand(rng) < probability_FH(s, loc_H)
             @info "actually doing local F -> H"
-            
-            # Get beta from neighbor at current time to use for colonized site
+
+            # Get beta from H site at current time to use for colonized site
             # (implicitly updates stored beta value)
-            beta = get_beta!(s, loc_neighbor)
-            set_state!(s, loc_F, H, TBetaPair(s.t, beta))
+            beta = get_beta!(s, loc_H)
+            set_state!(s, loc_neighbor, H, TBetaPair(s.t, beta))
             true
         else
-            @debug "not doing F -> H"
+            # @debug "not doing F -> H"
             false
         end
     else
@@ -566,32 +569,32 @@ function get_rate_global_FH(s)
 end
 
 function do_event_global_FH!(s, t)
-    @debug "do_event_global_FH!", t
-    
+    # @debug "do_event_global_FH!", t
+
     p = s.params
     rng = s.rng
-    
+
     # Draw a random forested site to be colonized
     @assert state_count(s, F) > 0
     loc_F = draw_location_in_state(s, F)
-    @debug "loc_F", loc_F
-    
+    # @debug "loc_F", loc_F
+
     # Draw a random human anywhere on the lattice
     @assert state_count(s, H) > 0
     loc_H = draw_location_in_state(s, H)
-    @debug "loc_H", loc_H
-    
+    # @debug "loc_H", loc_H
+
     # Perform event with probability that depends on neighbors of H site
     if rand(rng) < probability_FH(s, loc_H)
-        @debug "actually doing global F -> H"
-        
+        # @debug "actually doing global F -> H"
+
         # Get beta from human site at current time to use for colonized site
         # (implicitly updates stored beta value)
         beta = get_beta!(s, loc_H)
         set_state!(s, loc_F, H, beta)
         true
     else
-        @debug "not doing F -> H"
+        # @debug "not doing F -> H"
         false
     end
 end
@@ -626,19 +629,19 @@ function get_rate_AD(s::Simulation)
 end
 
 function do_event_AD!(s, t)
-    @debug "do_event_AD!", t
-    
+    # @debug "do_event_AD!", t
+
     p = s.params
     rng = s.rng
-    
+
     # Draw a random agricultural location
     @assert state_count(s, A) > 0
     loc = draw_location_in_state(s, A)
-    @debug "loc", loc
-    
+    # @debug "loc", loc
+
     # Perform event with probability_AD
     if rand(rng) < probability_AD(s, loc)
-        @debug "actually doing A -> D"
+        # @debug "actually doing A -> D"
         set_state!(s, loc, D)
         true
     else
@@ -660,19 +663,19 @@ function get_rate_HD(s)
 end
 
 function do_event_HD!(s, t)
-    @debug "do_event_HD!", t
-    
+    # @debug "do_event_HD!", t
+
     p = s.params
     rng = s.rng
-    
+
     # Draw a random human location
     @assert state_count(s, H) > 0
     loc = draw_location_in_state(s, H)
-    @debug "loc", loc
-    
+    # @debug "loc", loc
+
     # Perform event with probability_HD
     if rand(rng) < probability_HD(s, loc)
-        @debug "actually doing H -> D"
+        # @debug "actually doing H -> D"
         set_state!(s, loc, D)
         true
     else
@@ -689,41 +692,39 @@ end
 ### CONVERSION TO AGRICULTURE (F -> A) ###
 
 function get_rate_FA(s)
-    return state_count(s, F) * s.max_beta
+    return state_count(s, H) * s.max_beta
 end
 
 function do_event_FA!(s, t)
-    @debug "do_event_FA!", t
-    
+    # @debug "do_event_FA!", t
+
     p = s.params
     rng = s.rng
-    
-    # Draw a random forested site
-    @assert state_count(s, F) > 0
-    loc_F = draw_location_in_state(s, F)
-    site_F = get_site(s, loc_F)
-    @debug "loc_F", loc_F
-    
-    # Draw a random neighbor of the forested site
-    loc_neighbor = draw_neighbor(s, loc_F)
-    @debug "loc_neighbor", loc_neighbor
-    
-    # If neighbor is in state H, maybe transition to state A
-    @debug "neighbor state" get_state(s, loc_neighbor)
-    if get_state(s, loc_neighbor) == H
-        @debug "neighbor is H"
-        
-        # Observe beta for neighbor at current time
+
+    # Draw a random inhabited site
+    @assert state_count(s, H) > 0
+    loc_H = draw_location_in_state(s, H)
+
+    # Draw a random neighbor of the inhabited site
+    loc_neighbor = draw_neighbor(s, loc_H)
+    # @debug "loc_neighbor", loc_neighbor
+
+    # If neighbor is in state F, maybe transition to state A
+    # @debug "neighbor state" get_state(s, loc_neighbor)
+    if get_state(s, loc_neighbor) == F
+        # @debug "neighbor is F"
+
+        # Observe beta at current time
         # (implicitly updates stored value)
-        beta = get_beta!(s, loc_neighbor)
-        
+        beta = get_beta!(s, loc_H)
+
         # Perform event with probability beta / max_beta (rejection method)
         if rand(rng) < beta / s.max_beta
             @info "actually doing F -> A"
-            set_state!(s, loc_F, A)
+            set_state!(s, loc_neighbor, A)
             true
         else
-            @debug "not doing F -> A"
+            # @debug "not doing F -> A"
             false
         end
     else
@@ -739,16 +740,16 @@ function get_rate_DF(s)
 end
 
 function do_event_DF!(s, t)
-    @debug "do_event_DF!", t
-    
+    # @debug "do_event_DF!", t
+
     p = s.params
     rng = s.rng
-    
+
     # Draw a random degraded location
     @assert state_count(s, D) > 0
     loc = draw_location_in_state(s, D)
-    @debug "loc", loc
-    
+    # @debug "loc", loc
+
     set_state!(s, loc, F)
     true
 end
